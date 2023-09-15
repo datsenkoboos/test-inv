f583b430560794897b4bcd03b9f71684657e41ba
datsenkoboos <adacenkoboos@gmail.com>
2023-09-15 15:30:39 +0700

    updated itemIcon tests

--- src/components/ui/ItemIcon/ItemIcon.spec.ts
+++ src/components/ui/ItemIcon/ItemIcon.spec.ts
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, type ComponentMountingOptions } from '@vue/test-utils'
 import ItemIcon from './ItemIcon.vue'
 import { describe, expect, it } from 'vitest'
 
@@ -6,31 +6,41 @@ const itemIconSelector = '[data-testid=item-icon]'
 const itemIconMainPartSelector = '[data-testid=item-icon-main]'
 const itemIconBlurredPartSelector = '[data-testid=item-icon-blurred]'
 
-describe('ItemIcon', () => {
-  it('renders correctly with passed props', () => {
-    const color = 'blue'
-    const size = 20
+const defaultMountOptions: ComponentMountingOptions<typeof ItemIcon> = {
+  props: {
+    color: 'blue',
+    size: 20
+  }
+}
 
-    const wrapper = shallowMount(ItemIcon, {
-      props: {
-        color,
-        size
-      }
+describe('ItemIcon', () => {
+  describe('props', () => {
+    it('color - should render main icon part with set background color', () => {
+      const wrapper = shallowMount(ItemIcon, defaultMountOptions)
+      expect(wrapper.find(itemIconMainPartSelector).attributes('style')).toContain(
+        `background-color: ${defaultMountOptions.props!.color}`
+      )
     })
-    expect(wrapper.find(itemIconMainPartSelector).attributes('style')).toContain(
-      `background-color: ${color}`
-    )
-    expect(wrapper.find(itemIconMainPartSelector).attributes('style')).toContain(`width: ${size}px`)
-    expect(wrapper.find(itemIconMainPartSelector).attributes('style')).toContain(
-      `height: ${size}px`
-    )
-    expect(wrapper.find(itemIconBlurredPartSelector).attributes('style')).toContain(
-      `width: ${size}px`
-    )
-    expect(wrapper.find(itemIconBlurredPartSelector).attributes('style')).toContain(
-      `height: ${size}px`
-    )
-    expect(wrapper.find(itemIconSelector).attributes('style')).toContain(`width: ${1.1 * size}px`)
-    expect(wrapper.find(itemIconSelector).attributes('style')).toContain(`height: ${1.1 * size}px`)
+    it('size - should render icon and its parts with set size', () => {
+      const wrapper = shallowMount(ItemIcon, defaultMountOptions)
+      expect(wrapper.find(itemIconMainPartSelector).attributes('style')).toContain(
+        `width: ${defaultMountOptions.props!.size}px`
+      )
+      expect(wrapper.find(itemIconMainPartSelector).attributes('style')).toContain(
+        `height: ${defaultMountOptions.props!.size}px`
+      )
+      expect(wrapper.find(itemIconBlurredPartSelector).attributes('style')).toContain(
+        `width: ${defaultMountOptions.props!.size}px`
+      )
+      expect(wrapper.find(itemIconBlurredPartSelector).attributes('style')).toContain(
+        `height: ${defaultMountOptions.props!.size}px`
+      )
+      expect(wrapper.find(itemIconSelector).attributes('style')).toContain(
+        `width: ${1.1 * defaultMountOptions.props!.size}px`
+      )
+      expect(wrapper.find(itemIconSelector).attributes('style')).toContain(
+        `height: ${1.1 * defaultMountOptions.props!.size}px`
+      )
+    })
   })
 })
