8baf5b488903c1a69938899cc8727188da522f35
datsenkoboos <adacenkoboos@gmail.com>
2023-09-15 13:53:23 +0700

    moved initial data to separate file

---
+++ src/dataInit.ts
@@ -0,0 +1,9 @@
+import type { ItemStorage } from "./models";
+
+const dataInit: ItemStorage = {
+    0: { color: '#7faa65', quantity: 4 },
+    1: { color: '#aa9765', quantity: 2 },
+    2: { color: '#656caa', quantity: 5 }
+}
+
+export default dataInit;
