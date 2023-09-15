43d9b7a60dd4b8ea339865e0f495b0abf29b9623
datsenkoboos <adacenkoboos@gmail.com>
2023-09-15 10:22:12 +0700

    renamed side-section class to section

--- src/components/SideSection/SideSection.vue
+++ src/components/SideSection/SideSection.vue
@@ -1,26 +1,26 @@
 <template>
-  <aside class="side-section">
-    <div class="side-section-image-container">
+  <aside class="section">
+    <div class="section-image-container">
       <img
         src="@/assets/images/side-section-img.svg"
         alt="side-section-image"
-        class="side-section-image"
+        class="section-image"
       />
     </div>
-    <SkeletonLoader class="side-section-top-skeleton" />
-    <div class="side-section-description">
-      <SkeletonLoader v-for="_ in Array(5)" class="side-section-description-skeleton" />
+    <SkeletonLoader class="section-top-skeleton" />
+    <div class="section-description">
+      <SkeletonLoader v-for="_ in Array(5)" class="section-description-skeleton" />
     </div>
     <SkeletonLoader
-      class="side-section-description-skeleton side-section-description-skeleton-bottom"
+      class="section-description-skeleton section-description-skeleton-bottom"
     />
   </aside>
 </template>
 <script setup lang="ts">
 import { SkeletonLoader } from '@/components/ui'
 </script>
-<style lang="scss">
-.side-section {
+<style lang="scss" scoped>
+.section {
   width: 236px;
   padding: 18px 14px 24px;
   background-color: var(--color-bg-secondary);
