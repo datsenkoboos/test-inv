c9918f7bbf9dabac7ea074eab876101540b3db24
datsenkoboos <adacenkoboos@gmail.com>
2023-09-15 18:51:47 +0700

    added removeItem method on localstorage mock

--- src/mocks/LocalStorageMock.ts
+++ src/mocks/LocalStorageMock.ts
@@ -1,10 +1,11 @@
 import { vi } from 'vitest'
 
-const LocalStorageMock: Omit<Storage, 'length' | 'clear' | 'key' | 'removeItem'> = (() => {
+const LocalStorageMock: Omit<Storage, 'length' | 'clear' | 'key'> = (() => {
   const storage: { [key: string]: string } = {}
   return {
     getItem: vi.fn((key: string) => storage[key]),
-    setItem: vi.fn((key: string, value: string) => (storage[key] = value))
+    setItem: vi.fn((key: string, value: string) => (storage[key] = value)),
+    removeItem: vi.fn((key: string) => delete storage[key])
   }
 })()
 export default LocalStorageMock
